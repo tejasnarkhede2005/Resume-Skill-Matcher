11e1269f741582b5801debcb21320f0dc2947ad8
Tejas Narkhede <tejasnarkhede03@gmail.com>
2026-01-21 10:15:20 +0530

    Create skills.py

---
+++ skills.py
@@ -0,0 +1,6 @@
+SKILLS = [
+    "python", "sql", "machine learning", "data analysis",
+    "nlp", "deep learning", "excel", "power bi",
+    "tableau", "statistics", "regex", "streamlit",
+    "django", "flask", "pandas", "numpy"
+]
