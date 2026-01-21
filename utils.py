0094c504ea6d52d09f6dbb7ef5ae2560aedd7464
Tejas Narkhede <tejasnarkhede03@gmail.com>
2026-01-21 10:26:54 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,15 +1,13 @@
 import re
-import nltk
 from PyPDF2 import PdfReader
 from skills import SKILLS
-
-nltk.download('punkt')
 
 def extract_text_from_pdf(pdf):
     reader = PdfReader(pdf)
     text = ""
     for page in reader.pages:
-        text += page.extract_text()
+        if page.extract_text():
+            text += page.extract_text()
     return text.lower()
 
 def extract_skills(text):
@@ -24,3 +22,11 @@ def calculate_match(resume_skills, jd_skills):
     if not jd_skills:
         return 0
     return round((len(resume_skills & jd_skills) / len(jd_skills)) * 100, 2)
+
+def score_badge(score):
+    if score >= 80:
+        return "Excellent Match 🟢"
+    elif score >= 60:
+        return "Good Match 🟡"
+    else:
+        return "Needs Improvement 🔴"
